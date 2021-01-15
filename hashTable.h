ca77d5aff0e14907d946c846cd5d725fd8275397
tomer katz <64071448+tomerkatz2001@users.noreply.github.com>
2021-01-15 17:11:08 +0200

    Update hashTable.h

--- hashTable.h
+++ hashTable.h
@@ -1,16 +1,20 @@
+#ifndef HASH_TABLE 
+#define HASH_TABLE
+
+
 #include <bits/stdc++.h>
-#include "doubleArray.h" 
+#include "Array.h" 
 using namespace std; 
   
 class hashTable // a class for a Linked List implemantation of an hash table
 { 
     int size;    // amount of lists in the hash table 
-    list<DoubleArray> *table; //The hash table- maps course number to its "double array" that contains the lessons and some data about it
+    list<Array<int>*> *table; //The hash table- maps course number to its "double array" that contains the lessons and some data about it
 public: 
     hashTable(int size);  // Constructor 
   
     // insert a course into the hash table 
-    void insertItem(int course_number,DoubleArray array); 
+    void insertItem(int course_number,Array<int>* array); 
   
     // delete a course from the hash table 
     void deleteItem(int course_number); 
@@ -24,10 +28,10 @@ public:
 hashTable::hashTable(int size) 
 { 
     this->size =size; 
-    table = new list<DoubleArray>[size]; 
+    table = new list<Array<int>*>[size]; 
 } 
   
-void hashTable::insertItem(int course_number,DoubleArray array) 
+void hashTable::insertItem(int course_number,Array<int>* array) 
 { 
     int index = hashFunction(course_number); 
     table[index].push_back(array);  
@@ -39,9 +43,9 @@ void hashTable::deleteItem(int course_number)
   int index = hashFunction(course_number); 
   
   // find the course in the hash table
-  list <DoubleArray> :: iterator i; 
+  list <Array<int>*> :: iterator i; 
   for (i = table[index].begin(); i != table[index].end(); i++) { 
-    if ((*i).getCourseNumber() == course_number) 
+    if ((*i)->getCourseNum() == course_number) 
       break; 
   } 
   
@@ -49,5 +53,5 @@ void hashTable::deleteItem(int course_number)
   if (i != table[index].end()) 
     table[index].erase(i); 
 } 
-  
-  
+
+#endif
